e33878d74e042b881749ca9187fb1d2cb4d7e11a
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-04-16 16:56:13 +0530

    new update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,10 +6,9 @@ function Hero() {
     <section className=" bg-main py-[2rem] sm:py-[8rem]">
       <div className=" container-wrapper">
         <div className=" flex flex-col justify-center items-center pt-0 sm:pt-[1rem]">
-
-            <h1 className=" asul-bold text-xl sm:text-3xl font-semibold py-4">
+          <h1 className=" asul-bold text-xl sm:text-3xl font-semibold py-4">
             Laugh, stake, and collect rewards with Trump on Solana!
-            </h1>
+          </h1>
           <img
             src="images/logo.png"
             alt=""
@@ -17,29 +16,42 @@ function Hero() {
           />
 
           <div className="bg-[#ab3435] rounded-full py-2 px-4 my-3">
-            <a href="  https://solscan.io/token/7z9CYQFYBTXEZ4Tv6MhDAWvoeCbzSEAacd5Mc2bEU6kf" target="_blank" className="asul-bold text-xs sm:text-2xl">
-            7z9CYQFYBTXEZ4Tv6MhDAWvoeCbzSEAacd5Mc2bEU6kf
+            <a
+              href="#"
+              target="_blank"
+              className="asul-bold text-xs sm:text-2xl"
+            >
+              Ca:kfhkffmhgbnifsdhjfdmfnkjd
             </a>
           </div>
 
           <div className="flex justify-center gap-4 items-center">
-            <a href="https://www.dextools.io/app/en/solana/pair-explorer/7LVjXsZbMzEBkNj6BCRgp2LdSnRt3yavubmvgmvVJGtL?t=1710660493220" target="_blank" className="bg-[#ab3435] rounded-full text-lg sm:text-2xl py-2 px-6 font-bold asul-bold  border-2 border-black">
-            DEXTOOLS
+            <a
+              href="#"
+              target="_blank"
+              className="bg-[#ab3435] rounded-full text-lg sm:text-2xl py-2 px-6 font-bold asul-bold  border-2 border-black"
+            >
+              DEXTOOLS
             </a>
-
 
             <div className=" flex justify-center gap-4 items-center">
-            <a href="https://t.me/trumpsols" target="_blank" className=" text-5xl ">
-              <FaTelegram />
-            </a>
+              <a
+                href="https://t.me/trumpsols"
+                target="_blank"
+                className=" text-5xl "
+              >
+                <FaTelegram />
+              </a>
 
-            <a href="https://twitter.com/Trumphsol" target="_blank" className=" text-5xl">
-              <FaSquareXTwitter />
-            </a>
+              <a
+                href="https://twitter.com/Trumphsol"
+                target="_blank"
+                className=" text-5xl"
+              >
+                <FaSquareXTwitter />
+              </a>
+            </div>
           </div>
-          </div>
-
-         
         </div>
       </div>
     </section>
